74450eb463ee9739b2e3fea9331539bed7f453a1
Suralmk <surafelmelaku940@gmail.com>
2024-08-17 23:43:43 +0300

    updated main.py

--- backend/main.py
+++ backend/main.py
@@ -60,4 +60,4 @@ async def payment_process(request : Request ):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))
-    uvicorn.run("app.api:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
